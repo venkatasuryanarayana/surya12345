a5f9d5a9ace21a15f2467071bc1420fef1c49c09
venkatasuryanarayana <venkatasuryanarayana.n@apssdc.in>
2020-10-03 12:54:15 +0530

    Updates

---
+++ precache-manifest.0a2aa82928e17cba37e0654f4b9ff292.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda54bf282d91baa92c5979b84855b01",
+    "url": "/surya12345/index.html"
+  },
+  {
+    "revision": "3a682ce3a5c333a5c447",
+    "url": "/surya12345/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0babbde14e333bc689a2",
+    "url": "/surya12345/static/js/2.8aaac6e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/surya12345/static/js/2.8aaac6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a682ce3a5c333a5c447",
+    "url": "/surya12345/static/js/main.747041f8.chunk.js"
+  },
+  {
+    "revision": "b005fdaffef57583a62b",
+    "url": "/surya12345/static/js/runtime-main.4d5adecb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/surya12345/static/media/logo.5d5d9eef.svg"
+  }
+]);
